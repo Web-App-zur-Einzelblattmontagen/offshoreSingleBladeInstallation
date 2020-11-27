19706fb097be36bc789853f7e19f53d04925dde5
Zelgai123 <zelgainemati13@gmail.com>
2020-11-27 21:29:02 +0100

    setup finish

--- webApplication/reactpart/src/App.js
+++ webApplication/reactpart/src/App.js
@@ -1,25 +1,17 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="center-column">
+                <div className="item-row">
+                    <span>one</span>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
