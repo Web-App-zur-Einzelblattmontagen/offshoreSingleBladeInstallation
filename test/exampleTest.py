fa824fa8ea7a2c363b6b8613f6899eb8bd8b417a
Zelgai123 <zelgainemati13@gmail.com>
2020-11-27 21:00:52 +0100

    exampleTest insert

--- test/exampleTest.py
+++ test/exampleTest.py
@@ -1,10 +1,39 @@
 import unittest
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class TestStringMethods(unittest.TestCase):
 
+    def setUp(self):
+        pass
 
-if __name__ == '__main__':
-    unittest.main()
+    # Returns True if the string contains 4 a.
+    def test_strings_a(self):
+        self.assertEqual('a' * 4, 'aaaa')
+
+        # Returns True if the string is in upper case.
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+        # Returns TRUE if the string is in uppercase
+
+    # else returns False.
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+        # Returns true if the string is stripped and
+
+    # matches the given output.
+    def test_strip(self):
+        s = 'geeksforgeeks'
+        self.assertEqual(s.strip('geek'), 'sforgeeks')
+
+        # Returns true if the string splits and matches
+
+    # the given output.
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        with self.assertRaises(TypeError):
+            s.split(2)
