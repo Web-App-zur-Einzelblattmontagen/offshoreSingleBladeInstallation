535951e40560f289458cdbf96f8d63647a17be30
Zelgai123 <zelgainemati13@gmail.com>
2020-11-29 15:08:28 +0100

    sectionApart

--- webApplication/reactpart/src/App.js
+++ webApplication/reactpart/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './bik-logo.png';
 import bild1 from './bild1.png';
 import bild2 from './bild2.png';
+import bild3 from './bild3.png';
+import bild4 from './bild4.png';
 import './App.css';
 
 function App() {
@@ -11,7 +13,7 @@ function App() {
                 <img src={logo} alt="Logo"/>
                 <ul>
                     <li><a href="http://127.0.0.1:8000/">Startseite</a></li>
-                    <li><a href="http://127.0.0.1:8000/">Wissenschaftlicher Hintergrund</a></li>
+                    <li><a href="http://127.0.0.1:8000/">Schriftliche Ausarbeitung</a></li>
                     <li><a href="http://127.0.0.1:8000/">Impressum</a></li>
                 </ul>
             </nav>
@@ -51,6 +53,140 @@ function App() {
                         <img src={bild1} alt="Offshore Windanlagen Errichterschiff"/>
 
                         <img src={bild2} alt="Offshore Windanlage Einzelblattmontage"/>
+                    </div>
+                </div>
+                <br/>
+                <div className="row">
+                    <div className="col-3">
+                        <img className="biggerPicture" src={bild3} alt="Windpark Borkum Landkarte"/>
+
+                        <img className="biggerPicture" src={bild4} alt="Offshore Windrad"/>
+                    </div>
+                    <div className="col-3">
+
+                        <p className="Ueberschrift">Windpark Borkum 2</p>
+
+                        <p className="normalText">
+                            Offshore Wind liefert bereits heute einen substantiellen Teil des Energiemixes. Um die
+                            Installationskosten weiter zu senken, soll insbesondere der Installationsvorgang verbessert
+                            werden.
+                            Die Installation der Blätter stellt dabei die größte Herausforderung dar. Relativbewegungen
+                            zwischen Gondel und Blattwurzel erschweren die Blattmontage. Überschreitet die
+                            Relativbewegung einen gewissen Schwellwert, ist keine Installation mehr möglich und es kommt
+                            zu einer kostspieligen Verzögerung.
+                            Basierend auf Messdaten, welche während der Installation eines Windparks in der Nordsee
+                            aufgezeichnet wurden, sollen mittels maschineller Lernenmethoden Korrelationen zwischen
+                            Umweltgrößen, wie bspw. der Windgeschwindigkeit, und Beschleunigungen von Turm, Gondel und
+                            Blatt erkannt werden.
+                            In einem Softwaretechnikprojekt soll eine Web-App entwickelt werden, die, basierend auf
+                            aufgezeichneten Daten, Zusammenhängen ausgibt, sodass anschließend Ingenieure diese
+                            Ergebnisse für ein besseres Verständnis der Einzelblattmontage und ihre Umweltbedingungen
+                            nutzen können.
+                        </p>
+                    </div>
+                </div>
+                <br/>
+                <div className="row">
+                    <div className="col-3">
+
+                        <p className="Ueberschrift">Messdatenaufnahme</p>
+
+                        <p className="normalText">
+                            Offshore Wind liefert bereits heute einen substantiellen Teil des Energiemixes. Um die
+                            Installationskosten weiter zu senken, soll insbesondere der Installationsvorgang
+                            verbessert
+                            werden.
+                            Die Installation der Blätter stellt dabei die größte Herausforderung dar.
+                            Relativbewegungen
+                            zwischen Gondel und Blattwurzel erschweren die Blattmontage. Überschreitet die
+                            Relativbewegung einen gewissen Schwellwert, ist keine Installation mehr möglich und es
+                            kommt
+                            zu einer kostspieligen Verzögerung.
+                            Basierend auf Messdaten, welche während der Installation eines Windparks in der Nordsee
+                            aufgezeichnet wurden, sollen mittels maschineller Lernenmethoden Korrelationen zwischen
+                            Umweltgrößen, wie bspw. der Windgeschwindigkeit, und Beschleunigungen von Turm, Gondel
+                            und
+                            Blatt erkannt werden.
+                            In einem Softwaretechnikprojekt soll eine Web-App entwickelt werden, die, basierend auf
+                            aufgezeichneten Daten, Zusammenhängen ausgibt, sodass anschließend Ingenieure diese
+                            Ergebnisse für ein besseres Verständnis der Einzelblattmontage und ihre
+                            Umweltbedingungen
+                            nutzen können.
+                        </p>
+
+
+                        <img src={bild1} alt="Offshore Windanlagen Errichterschiff"/>
+
+                        <img src={bild2} alt="Offshore Windanlage Einzelblattmontage"/>
+
+                    </div>
+
+
+                    <div className="col-3">
+
+
+                        <p className="Ueberschrift">Messdatenverarbeitung</p>
+
+                        <p className="normalText">
+                            Offshore Wind liefert bereits heute einen substantiellen Teil des Energiemixes. Um die
+                            Installationskosten weiter zu senken, soll insbesondere der Installationsvorgang
+                            verbessert
+                            werden.
+                            Die Installation der Blätter stellt dabei die größte Herausforderung dar.
+                            Relativbewegungen
+                            zwischen Gondel und Blattwurzel erschweren die Blattmontage. Überschreitet die
+                            Relativbewegung einen gewissen Schwellwert, ist keine Installation mehr möglich und es
+                            kommt
+                            zu einer kostspieligen Verzögerung.
+                            Basierend auf Messdaten, welche während der Installation eines Windparks in der Nordsee
+                            aufgezeichnet wurden, sollen mittels maschineller Lernenmethoden Korrelationen zwischen
+                            Umweltgrößen, wie bspw. der Windgeschwindigkeit, und Beschleunigungen von Turm, Gondel
+                            und
+                            Blatt erkannt werden.
+                            In einem Softwaretechnikprojekt soll eine Web-App entwickelt werden, die, basierend auf
+                            aufgezeichneten Daten, Zusammenhängen ausgibt, sodass anschließend Ingenieure diese
+                            Ergebnisse für ein besseres Verständnis der Einzelblattmontage und ihre
+                            Umweltbedingungen
+                            nutzen können.
+                        </p>
+                        <img src={bild1} alt="Offshore Windanlagen Errichterschiff"/>
+
+                        <img src={bild2} alt="Offshore Windanlage Einzelblattmontage"/>
+
+                    </div>
+                </div>
+                <br/>
+                <div className="row">
+                    <div className="col-3">
+
+                        <p className="Ueberschrift">Uploadbereich</p>
+
+                        <p className="normalText">
+                            Offshore Wind liefert bereits heute einen substantiellen Teil des Energiemixes. Um die
+                            Installationskosten weiter zu senken, soll insbesondere der Installationsvorgang verbessert
+                            werden.
+                            Die Installation der Blätter stellt dabei die größte Herausforderung dar. Relativbewegungen
+                            zwischen Gondel und Blattwurzel erschweren die Blattmontage. Überschreitet die
+                            Relativbewegung einen gewissen Schwellwert, ist keine Installation mehr möglich und es kommt
+                            zu einer kostspieligen Verzögerung.
+                            Basierend auf Messdaten, welche während der Installation eines Windparks in der Nordsee
+                            aufgezeichnet wurden, sollen mittels maschineller Lernenmethoden Korrelationen zwischen
+                            Umweltgrößen, wie bspw. der Windgeschwindigkeit, und Beschleunigungen von Turm, Gondel und
+                            Blatt erkannt werden.
+                            In einem Softwaretechnikprojekt soll eine Web-App entwickelt werden, die, basierend auf
+                            aufgezeichneten Daten, Zusammenhängen ausgibt, sodass anschließend Ingenieure diese
+                            Ergebnisse für ein besseres Verständnis der Einzelblattmontage und ihre Umweltbedingungen
+                            nutzen können.
+                        </p>
+                    </div>
+                    <div className="col-3">
+                        <form>
+                            <label>
+                                Datei hochladen:
+                                <input type="file" name="file"/>
+                            </label>
+                            <input type="submit" value="Absenden"/>
+                        </form>
                     </div>
                 </div>
 
