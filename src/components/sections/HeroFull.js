8498c6918579b4aeee52661b9da984171afea760
Zelgai123 <59771046+Zelgai123@users.noreply.github.com>
2021-01-25 10:22:49 +0100

    Update HeroFull.js

--- src/components/sections/HeroFull.js
+++ src/components/sections/HeroFull.js
@@ -50,7 +50,7 @@ class HeroFull extends React.Component {
                     <div className={innerClasses}>
                         <div className="hero-content">
                             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-                                Offshore Single Blade Installation
+                                Offshore Single Blade Installation test
                             </h1>
                             <div className="container-xs">
                                 <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
