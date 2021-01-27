6d72634ceebc2a047a2ba1cd8cc7ebcb136a936b
Zelgai123 <zelgainemati13@gmail.com>
2021-01-27 19:23:12 +0100

    adding build folder for cd script

---
+++ build/precache-manifest.f47e5b701a25737112c5e9d73f1348d4.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17569a2ba1737b109d54cadda57a24cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5eca7a2d167bf8e8aabf",
+    "url": "/static/css/main.520e6061.chunk.css"
+  },
+  {
+    "revision": "1d839d490ae62c1d5a64",
+    "url": "/static/js/2.ca64c523.chunk.js"
+  },
+  {
+    "revision": "5eca7a2d167bf8e8aabf",
+    "url": "/static/js/main.50ccb7b9.chunk.js"
+  },
+  {
+    "revision": "4bbca46a7ab6969d2e25",
+    "url": "/static/js/runtime~main.1fdfdb48.js"
+  },
+  {
+    "revision": "5de044bf68915cb7874a6212090aeaf0",
+    "url": "/static/media/bikLogo.5de044bf.svg"
+  },
+  {
+    "revision": "986650de73c4319ffd12582ffaaa769a",
+    "url": "/static/media/black.986650de.jpg"
+  },
+  {
+    "revision": "60c45c8f2f3fee03ff02e9e55dca693f",
+    "url": "/static/media/clients-01.60c45c8f.svg"
+  },
+  {
+    "revision": "ccd24bd063bc8ad37b695fb06c1f9e92",
+    "url": "/static/media/clients-02.ccd24bd0.svg"
+  },
+  {
+    "revision": "333c569a563014533edc2d903704bbc0",
+    "url": "/static/media/clients-03.333c569a.svg"
+  },
+  {
+    "revision": "1e121c928fb7f7156460826bd08d4c24",
+    "url": "/static/media/clients-04.1e121c92.svg"
+  },
+  {
+    "revision": "d09fba01d705ce37ac07e6bea2a088d2",
+    "url": "/static/media/clients-05.d09fba01.svg"
+  },
+  {
+    "revision": "a1e42224a1d08502dcd016a5aa0020ce",
+    "url": "/static/media/feature-tile-icon-01.a1e42224.svg"
+  },
+  {
+    "revision": "cbb70dccd036f8f898991599ed2e3649",
+    "url": "/static/media/feature-tile-icon-02.cbb70dcc.svg"
+  },
+  {
+    "revision": "48091a1f7c0258ee659aae4c9ebacc61",
+    "url": "/static/media/feature-tile-icon-03.48091a1f.svg"
+  },
+  {
+    "revision": "dba41e956ad278e390526f11282399a3",
+    "url": "/static/media/feature-tile-icon-04.dba41e95.svg"
+  },
+  {
+    "revision": "3c287c722597f16634fe287b302e2ae0",
+    "url": "/static/media/feature-tile-icon-05.3c287c72.svg"
+  },
+  {
+    "revision": "3c7d3b102b1fb0779d299033143a2081",
+    "url": "/static/media/feature-tile-icon-06.3c7d3b10.svg"
+  },
+  {
+    "revision": "4c66333f28a69b611caa3ab78e437a0b",
+    "url": "/static/media/illustration-element-01.4c66333f.svg"
+  },
+  {
+    "revision": "ad9b81a8361d1ca5d6b49b80dd00035e",
+    "url": "/static/media/illustration-section-01.ad9b81a8.svg"
+  },
+  {
+    "revision": "3f0d2984a80822ae99c43d63202fd458",
+    "url": "/static/media/image-placeholder.3f0d2984.png"
+  },
+  {
+    "revision": "6fe955869498f39e33bd4f8bb622187c",
+    "url": "/static/media/team-member-01.6fe95586.jpg"
+  },
+  {
+    "revision": "7c2123864ec07757aefcd3d499ac2a05",
+    "url": "/static/media/team-member-03.7c212386.jpg"
+  },
+  {
+    "revision": "ff17092313f3400453617a0805badfce",
+    "url": "/static/media/team-member-04.ff170923.jpg"
+  },
+  {
+    "revision": "1f6ff88a64d9cb4a97f97bd254e62000",
+    "url": "/static/media/team-member-05.1f6ff88a.jpg"
+  },
+  {
+    "revision": "c34d3ad5dd9e120886e3fcfce0d6521c",
+    "url": "/static/media/titlePicture.c34d3ad5.bin"
+  }
+]);
